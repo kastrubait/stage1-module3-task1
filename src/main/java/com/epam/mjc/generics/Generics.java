83938455af246a7acf6e71ee43bb52e0ba17e6b3
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-11-26 12:03:01 +0300

    fix: complete task1 module3 stage1 v.1

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Generics {
 
     //TODO: Refactor Method-1
-    public List<String> boxingMethod(String name) {
-        List<String> firstList = new ArrayList<String>();
+    public List<List<String>> boxingMethod(String name) {
+        List<String> firstList = new ArrayList<>();
         firstList.add(name);
-        List<String> secondList = new ArrayList<String>();
-        secondList.add(String.valueOf(firstList));
+        List<List<String>> secondList = new ArrayList<>();
+        secondList.add(firstList);
         return secondList;
     }
 
@@ -20,7 +20,7 @@ public class Generics {
     }
 
     //TODO: Refactor Method-3
-    public <T> void cloneMethod(List<T> consumer, List<? extends T> producer) {
+    public <T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
